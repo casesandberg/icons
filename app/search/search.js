d7e43f5266236c97a7d61150f6c6c9f90fa33647
casesandberg <case@casesandberg.com>
2014-10-10 22:04:11 -0700

    Search more efficient

--- app/search/search.js
+++ app/search/search.js
@@ -2,27 +2,22 @@
 
 angular.module('search', [])
 
-.controller('SearchCtrl', ['$scope', '$firebase', '$window', '$http', function($scope, $firebase, $window, $http){
+.controller('SearchCtrl', ['$scope', '$firebase', '$http', function($scope, $firebase, $http){
 
     $http.get('app/search/icons.json')
-        .then(function(res){
-            $scope.icons = res.data.icons;
+    .then(function(res){
+        var filtered = [];
+        angular.forEach(res.data.icons, function(item) {
+            filtered.push(item);
         });
+       $scope.icons = filtered;
+    });
 
     $scope.searchText = '';
+    $scope.selectedIcons = [];
 
     // var iconsRef = $firebase(new Firebase('https://icons.firebaseio.com/'));
     // $scope.icons = iconsRef.$child('icons');
-    //
-    // $scope.icons.$on('loaded', function(){
-    //
-    //     angular.forEach($scope.icons, function(obj, index) {
-    //         if($scope.icons[index].href){
-    //             $scope.icons[index].filename = $scope.icons[index].href.replace(/[0-9]*-/, '').replace(/-/g, ' ').replace(/(@2x.svg)/, '')
-    //         }
-    //     });
-    //
-    // });
 
     $scope.addToTray = function(obj){
         var newObj = {
@@ -32,56 +27,8 @@ angular.module('search', [])
         $scope.selectedIcons.push(newObj);
     }
 
-    $scope.selectedIcons = [];
-
     $scope.clear = function(){
         $scope.selectedIcons = [];
     }
 
 }])
-
-.filter('clean', function() {
-    return function(items) {
-    return items.replace(/[0-9]*-/, '').replace(/-/g, ' ').replace(/(@2x.svg)/, '');
-    };
-})
-
-.filter('array', function() {
-  return function(items) {
-    var filtered = [];
-    angular.forEach(items, function(item) {
-      filtered.push(item);
-    });
-   return filtered;
-  };
-});
-
-// .directive('walletSwitcher', ['$location', '$timeout', function($location, $timeout) {
-//     return {
-//         restrict:'A',
-//         controller: 'WalletCtrl',
-//         link: function(scope, element, attrs) {
-//
-//             scope.$watch('active', function(newV, oldV){
-//
-//                 if(scope.active == 'usd'){
-//                     scope.total = '39.20';
-//                     scope.symbol = '$';
-//                 }
-//                 if(scope.active == 'btc'){
-//                     scope.total = '0.0192';
-//                     scope.symbol = '' // '฿';
-//                 }
-//
-//                 if (newV != oldV){
-//
-//                     element.addClass('animate-total');
-//                     $timeout(function(){
-//                         element.removeClass('animate-total');
-//                     }, 200);
-//                 }
-//             })
-//
-//         }
-//     }
-// }])
